8e82b3aa95209ca015e77c29e29fbb45ff3a66b1
hikazudani <lhjundi@proton.me>
2026-05-04 11:55:01 -0300

    feat: add numeric Preview in LabeledTextField

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/ui/components/LabeledTextField.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/ui/components/LabeledTextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,5 +39,16 @@ private fun LabeledTextFieldPreview() {
         label = "Destino",
         value = "São Carlos",
         onValueChange = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LabeledTextFieldNumericPreview() {
+    LabeledTextField(
+        label = "Dias",
+        value = "7",
+        onValueChange = {},
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
 }
