ab6587cbcb42592d78350b267bf3c07a0b405982
hikazudani <lhjundi@proton.me>
2026-05-04 13:17:11 -0300

    feat: add Primary Button component

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/ui/components/PrimaryButton.kt
@@ -0,0 +1,33 @@
+package br.edu.ifsp.scl.sc3038432.fasttripplanner.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        enabled = enabled
+    ) {
+        Text( text = text )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PrimaryButtonPreview(){
+    PrimaryButton(
+        text = "Proximo",
+        onClick = {}
+    )
+}
