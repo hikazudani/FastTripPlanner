36906b30c8e3ded35dc6c963fb214848622db76b
hikazudani <lhjundi@proton.me>
2026-05-04 20:07:06 -0300

    refactor: remove unused imports

--- app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/TripInputActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/TripInputActivity.kt
@@ -4,16 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3038432.fasttripplanner.ui.screens.TripInputScreen
-import br.edu.ifsp.scl.sc3038432.fasttripplanner.ui.theme.FastTripPlannerTheme
+
 
 class TripInputActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
