5ca7b9466de85cca50ae06f5269b8a4e4becadf9
hikazudani <lhjundi@proton.me>
2026-05-04 11:51:08 -0300

    feat: create LabeledTextField component with Preview

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3038432/fasttripplanner/ui/components/LabeledTextField.kt
@@ -0,0 +1,42 @@
+package br.edu.ifsp.scl.sc3038432.fasttripplanner.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LabeledTextField(
+    label: String,
+    value: String,
+    onValueChange: ( String ) -> Unit,
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default // Allows defining the keyboard type
+) {
+    Column( modifier = modifier ) {
+        Text( text = label, modifier = Modifier.padding( bottom = 4.dp) )
+        TextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            keyboardOptions = keyboardOptions
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LabeledTextFieldPreview() {
+    LabeledTextField(
+        label = "Destino",
+        value = "São Carlos",
+        onValueChange = {}
+    )
+}
